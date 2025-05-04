0113ff94c1dc1d2039985d0dad9954b04776dc70
Nicola <nicola.laubscher10@gmail.com>
2025-05-04 20:42:04 +0200

    pipeline PV updated

--- Data_PV/config.py
+++ Data_PV/config.py
@@ -19,7 +19,7 @@ analysis = config.analysis
 solver = config.solver
 
 ## Analysis - settings update compared to default values
-analysis["dataset"] = os.path.join(os.path.dirname(__file__), "01_SP_MV_PV")
+analysis["dataset"] = os.path.join(os.path.dirname(__file__), "01_SP_MV_PV_new")
 analysis["objective"] = "total_cost"
 # use greenfield or brownfield approach
 analysis["use_capacities_existing"] = True
